e10adc461fdc6776f728feddb5dc3c55d8f7baa0
“Anastasia <ntabaharnuk@gmail.com>
2022-11-11 16:56:13 +0200

    сортування

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function text(){
-    const figure = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const text = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
-    const a = [];
-  
-    for(let i = 0; i < arguments.length; i++) {
-        a[i] = text[arguments[i]];
-    }
-    return a;
-  }
-  
-  console.log(text(7, 2, 4, 0));
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+/* Сортування слів */ 
+let lettersort = ['home', 'work', 'relax', 'hollidays', 'job', 'city']
+lettersort.sort()
+console.log(lettersort);
+
+
+/* Сортування від меншого до більшого значення */
+let NumberSortMinToMax = [1, 67, 334, 0, 239, 2, 34, 98, 34, 2334]
+NumberSortMinToMax.sort(compareNumbers)
+console.log(NumberSortMinToMax);
+
+
+/* Сортування від більшого до меншого значення */
+let NumberSortMaxToMin = [89, 6, 0, 94, 54, 50, 23, 34]
+NumberSortMaxToMin.reverse(NumberSortMaxToMin.sort(compareNumbers))
+console.log(NumberSortMaxToMin);
